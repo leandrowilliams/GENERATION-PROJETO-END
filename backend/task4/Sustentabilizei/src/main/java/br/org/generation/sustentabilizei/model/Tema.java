d77c7b7c34e8268b4e7ca724da2efe4facc9c8de
leandrowilliams <87885115+leandrowilliams@users.noreply.github.com>
2021-11-04 13:53:50 -0300

    task4

--- backend/task4/Sustentabilizei/src/main/java/br/org/generation/sustentabilizei/model/Tema.java
+++ backend/task4/Sustentabilizei/src/main/java/br/org/generation/sustentabilizei/model/Tema.java
@@ -19,7 +19,15 @@ public class Tema {
 	
 	@NotNull(message = "Por favor, insira a descrição!!! ")
 	@Size (max = 500)
+	private String tema;
+	
+	@NotNull(message = "Por favor, insira a descrição!!! ")
+	@Size (max = 500)
 	private String descricao;
+	
+	@NotNull(message = "Por favor, insira a descrição!!! ")
+	@Size (max = 255)
+	private String palavraChave;
 
 	public long getId() {
 		return id;
@@ -29,6 +37,14 @@ public class Tema {
 		this.id = id;
 	}
 
+	public String getTema() {
+		return tema;
+	}
+
+	public void setTema(String tema) {
+		this.tema = tema;
+	}
+
 	public String getDescricao() {
 		return descricao;
 	}
@@ -36,7 +52,15 @@ public class Tema {
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
-	
-	
+
+	public String getPalavraChave() {
+		return palavraChave;
+	}
+
+	public void setPalavraChave(String palavraChave) {
+		this.palavraChave = palavraChave;
+	}
+
+		
 	
 }
