42abf3db01977762771eb1a6ad982e17576f8b56
leandrowilliams <87885115+leandrowilliams@users.noreply.github.com>
2021-11-04 11:10:15 -0300

    task4

---
+++ backend/task4/Sustentabilizei/src/main/java/br/org/generation/sustentabilizei/SustentabilizeiApplication.java
@@ -0,0 +1,13 @@
+package br.org.generation.sustentabilizei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SustentabilizeiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SustentabilizeiApplication.class, args);
+	}
+
+}
---
+++ backend/task4/Sustentabilizei/src/test/java/br/org/generation/sustentabilizei/SustentabilizeiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.generation.sustentabilizei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SustentabilizeiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
