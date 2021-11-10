b5f554118c2bc7a199d1dbfce2f66075d8033050
linghel marla <linghel.marlacms@gmail.com>
2021-11-10 15:22:51 -0300

    Criaçao da Model Usuario e Postagens

---
+++ backend/Task 5/Sustentabilizei/src/main/java/br/org/generation/sustentabilizei/model/Postagens.java
@@ -0,0 +1,98 @@
+package br.org.generation.sustentabilizei.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_postagens")
+public class Postagens {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@NotNull(message = " O atributo Titulo é obrigatório!")
+	@Size(min = 5, max = 50, message = "O Titulo Deve conter no minimo 5 caracteres e no maximo 50 caracteres.")
+	private String titulo;
+
+	@NotNull(message = " O atributo conteudo é obrigatório!")
+	@Size(max = 500, message = "O conteudo Deve conter no maximo 50 caracteres.")
+	private String conteudo;
+
+	private String midia;
+
+	private String hashtag;
+
+	@ManyToOne
+	@JsonIgnoreProperties("postagens")
+	private Tema tema;
+
+	@ManyToOne
+	@JsonIgnoreProperties("usuario")
+	private Usuario usuario;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getConteudo() {
+		return conteudo;
+	}
+
+	public void setConteudo(String conteudo) {
+		this.conteudo = conteudo;
+	}
+
+	public String getMidia() {
+		return midia;
+	}
+
+	public void setMidia(String midia) {
+		this.midia = midia;
+	}
+
+	public String getHashtag() {
+		return hashtag;
+	}
+
+	public void setHashtag(String hashtag) {
+		this.hashtag = hashtag;
+	}
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+}
